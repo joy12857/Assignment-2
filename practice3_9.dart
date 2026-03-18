9b4f2bd794d8baf0a325abf3374384540ea666a7
joy12857 <chowdhuryjoy128@gmail.com>
2026-03-18 22:43:49 +0600

    Delete practice3_9.dart

--- practice3_9.dart
+++
@@ -1,7 +0,0 @@
-int maxNumber(int a, int b, int c) {
-  return [a, b, c].reduce((value, element) => value > element ? value : element);
-}
-
-void main() {
-  print("Largest number: ${maxNumber(10, 25, 7)}");
-}
