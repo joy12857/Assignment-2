b6fc4fc8e11a6d9889627fd818143cb60daf04f3
joy12857 <chowdhuryjoy128@gmail.com>
2026-03-18 22:31:33 +0600

    Update and rename Prac6_Q3.dart to practice6_3.dart

--- Prac6_Q3.dart
+++
@@ -1,8 +0,0 @@
-// Question 3:
-// Write a dart program to create an enum class for gender [male, female, others] and print all values.
-
-enum Gender { male, female, others }
-
-void main() {
-  print('All Genders: ${Gender.values}');
-}
